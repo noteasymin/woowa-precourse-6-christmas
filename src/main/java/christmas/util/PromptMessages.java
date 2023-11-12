11c91e04b86d5a7c0c2d80e4f511a9d4478317b4
noteasymin <noteasymin@gmail.com>
2023-11-13 03:32:57 +0900

    refactor: 프롬포트 메시지 상수 처리

---
+++ src/main/java/christmas/util/PromptMessages.java
@@ -0,0 +1,5 @@
+package christmas.util;
+
+public class PromptMessages {
+    public static final String DATE_PROMPT = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,11 +2,12 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.exception.ErrorMessages;
+import christmas.util.PromptMessages;
 
 public class InputView {
     public int readDate() {
         while (true) {
-            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            System.out.println(PromptMessages.DATE_PROMPT);
             String input = Console.readLine();
 
             try {
