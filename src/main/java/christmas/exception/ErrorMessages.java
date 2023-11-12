36266d7b8cc7bdc890dac67b17d6f4b548d7265d
noteasymin <noteasymin@gmail.com>
2023-11-13 04:00:38 +0900

    refactor: 에러 메시지 내용 수정

--- src/main/java/christmas/exception/ErrorMessages.java
+++ src/main/java/christmas/exception/ErrorMessages.java
@@ -1,6 +1,6 @@
 package christmas.exception;
 
 public class ErrorMessages {
-    public static final String INVALID_NUMBER = "[ERROR] 숫자를 입력해 주세요.";
-    public static final String INVALID_DATE_RANGE = "[ERROR] 1부터 31 사이의 숫자를 입력해 주세요.";
+    public static final String INVALID_NUMBER = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String INVALID_DATE_RANGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 }
