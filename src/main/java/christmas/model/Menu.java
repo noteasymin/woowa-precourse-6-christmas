b29d4be78f0cf727d9c4ffdbc65386290b36b1d4
noteasymin <noteasymin@gmail.com>
2023-11-14 01:58:10 +0900

    feat: 메뉴 객체 생성

---
+++ src/main/java/christmas/model/Menu.java
@@ -0,0 +1,42 @@
+package christmas.model;
+
+public enum Menu {
+    MUSHROOM_SOUP("양송이수프", 6000, Category.APPETIZER),
+    TAPAS("타파스", 5000, Category.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8000, Category.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55000, Category.MAIN),
+    BBQ_RIBS("바비큐립", 54000, Category.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35000, Category.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, Category.MAIN),
+    CHOCOLATE_CAKE("초코케이크", 15000, Category.DESSERT),
+    ICE_CREAM("아이스크림", 5000, Category.DESSERT),
+    ZERO_COKE("제로콜라", 3000, Category.DRINK),
+    RED_WINE("레드와인", 60000, Category.DRINK),
+    CHAMPAGNE("샴페인", 25000, Category.DRINK);
+
+    private final String name;
+    private final int price;
+    private final Category category;
+
+    Menu(String name, int price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public enum Category {
+        APPETIZER, MAIN, DESSERT, DRINK
+    }
+}
