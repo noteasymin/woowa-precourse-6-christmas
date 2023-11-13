e9d6edb91f1eccd7335074e6beb6421f0d663b89
noteasymin <noteasymin@gmail.com>
2023-11-14 03:45:42 +0900

    refactor: 오더 객체 수정

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,19 @@
+package christmas.model;
+
+public class Order {
+    private String menuName;
+    private int quantity;
+
+    public Order(String menuName, int quantity) {
+        this.menuName = menuName;
+        this.quantity = quantity;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
