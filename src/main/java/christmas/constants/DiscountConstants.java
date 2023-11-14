e5550dae7c2b0a88cde6f881e48fe226aebadd64
noteasymin <noteasymin@gmail.com>
2023-11-15 02:11:59 +0900

    refactor: constans 패키지 생성

---
+++ src/main/java/christmas/constants/DiscountConstants.java
@@ -0,0 +1,9 @@
+package christmas.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountConstants {
+    public static final List<Integer> SPECIAL_DISCOUNT_DAYS = Arrays.asList(3, 10, 17, 24, 25, 31);
+    public static final int SPECIAL_DISCOUNT_AMOUNT = 1000;
+}
